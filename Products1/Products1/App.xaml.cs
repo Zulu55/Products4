5e540ea377b662cc01b3a5d5a563068967ec88ae
Zulu <jzuluaga55@gmail.com>
2017-09-14 19:52:01 -0500

    Login begin

--- Products1/Products1/App.xaml.cs
+++ Products1/Products1/App.xaml.cs
@@ -1,19 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Xamarin.Forms;
-
-namespace Products1
+﻿namespace Products1
 {
+    using Xamarin.Forms;
+    using Views;
+
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
 
-            MainPage = new Products1.MainPage();
+            MainPage = new NavigationPage(new LoginView());
         }
 
         protected override void OnStart()
---
+++ Products1/Products1/ViewModels/LoginViewModel.cs
@@ -0,0 +1,157 @@
+﻿namespace Products1.ViewModels
+{
+    using System.ComponentModel;
+    using System.Windows.Input;
+    using GalaSoft.MvvmLight.Command;
+    using Services;
+
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region Services
+        DialogService dialogService;
+        #endregion
+
+        #region Attibutes
+        string _email;
+        string _password;
+        bool _isToggled;
+        bool _isRunning;
+        bool _isEnabled;
+        #endregion
+
+        #region Properties
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                if (_email != value)
+                {
+                    _email = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(Email)));
+                }
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                if (_password != value)
+                {
+                    _password = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(Password)));
+                }
+            }
+        }
+
+        public bool IsToggled
+        {
+            get
+            {
+                return _isToggled;
+            }
+            set
+            {
+                if (_isToggled != value)
+                {
+                    _isToggled = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(IsToggled)));
+                }
+            }
+        }
+
+        public bool IsRunning
+        {
+            get
+            {
+                return _isRunning;
+            }
+            set
+            {
+                if (_isRunning != value)
+                {
+                    _isRunning = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(IsRunning)));
+                }
+            }
+        }
+
+        public bool IsEnabled
+        {
+            get
+            {
+                return _isEnabled;
+            }
+            set
+            {
+                if (_isEnabled != value)
+                {
+                    _isEnabled = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(IsEnabled)));
+                }
+            }
+        }
+        #endregion
+
+        #region Constructors
+        public LoginViewModel()
+        {
+            dialogService = new DialogService();
+
+            IsEnabled = true;
+            IsToggled = true;
+        }
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+        async void Login()
+        {
+            if (string.IsNullOrEmpty(Email))
+            {
+                await dialogService.ShowMessage(
+                    "Error",
+                    "You must enter an email.");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(Password))
+            {
+                await dialogService.ShowMessage(
+                    "Error",
+                    "You must enter a password.");
+                return;
+            }
+        }
+        #endregion
+    }
+}
